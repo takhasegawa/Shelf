b6ae7afc5e82194b3798250c89bf67f9ba9eb563
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-02-18 21:35:24 +0900

    2巻 8章まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,12 @@
 class BooksController < ApplicationController
+  before_filter :prepare, :only => [ :index, :checked_out]
 
   def index
-    if params[:category_id]
-      @category = Category.find(params[:category_id])
-      @books = @category.books.checked_in.paginate(:page => params[:page], :per_page =>20)
-    else
-      @books = Book.checked_in.paginate(:page => params[:page], :per_page =>20)
-    end
+    @books = @books.checked_in.paginate(:page => params[:page], :per_page =>20)
   end
   
   def checked_out
-    if params[:category_id]
-      @category = Category.find(params[:category_id])
-      @books = @category.books.checked_out.paginate(:page => params[:page], :per_page => 20)
-    else
-      @books = Book.checked_out.paginate(:page => params[:page], :per_page => 20)
-    end
+    @books = @books.checked_out.paginate(:page => params[:page], :per_page => 20)
     render :index
   end
   
@@ -71,5 +62,16 @@ class BooksController < ApplicationController
     @books = Book.search(params[:query])
     @books = @books.paginate(:page => params[:page], :per_page => 20)
     render :index
+  end
+
+  private
+
+  def prepare
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @books = @category.books
+    else
+      @books = Book
+    end  
   end
 end
