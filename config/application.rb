4f461bcec867a512fbab478dc5f3c6e1fa5b70ee
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-03-07 00:52:57 +0900

    第2巻12章まで

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Shelf
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_controller.include_all_helpers = false
   end
 end
