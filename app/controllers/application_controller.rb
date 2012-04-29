fe3d68be45141894a8a0aaed5c23f0656411015c
tak.hasegawa <tak.es.hasegawa+github@gmail.com>
2012-04-29 23:41:18 +0900

    第2巻 終了時点まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from Exception, :with => :render_500
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  
+private
+  def render_404(exception)
+    render "errors/not_found", :status => 404
+  end
+  
+  def render_500(exception)
+    render "errors/internal_server_error", :status => 500
+  end
+
 end
